51c4c268ac7b5c9ec51d94439e435d756e351a1f
Alan Gonçalves <biscanas4@gmail.com>
2021-08-06 16:47:54 -0300

    photos
    added photos and little fixes...
    js codes and remove some file in the counter..

--- script.js
+++ script.js
@@ -1,53 +1,69 @@
+var agr = new Date()
+var hora = agr.getHours()
+var escreva = document.querySelector('div#espaço-data')
+var insira = document.querySelector('img#foto')
+var a = new Date()
+var diaSem = a.getDay()
+
 function carregar() {
-    var agr = new Date()
-    var hora = agr.getHours()
-    var escreva = document.querySelector('div#espaço-data')
     
     //document.body.style.background-Image.linear-gradient            
 
-    escreva.innerHTML = `Agora são exatamente ${hora} horas.`
-    if (hora<12&&hora>=6) {
-        escreva.innerHTML += ' Bom dia!'
-        document.body.style.background = '#B54E61'
-    } else if (hora>=12&&hora<18) {
-        escreva.innerHTML += ' Boa tarde!'
-        document.body.style.background = '#6B4669'
-    } else if (hora>=18&&hora<=23) {
-        escreva.innerHTML += ' Boa noite!'
-        document.body.style.background = '#3E3E67'
-    } else {
-        escreva.innerHTML += ' Boa madrugada!'
-        document.body.style.background = '#FF8944'
-    }
-}
-
-function carregar() {
-    var a = new Date()
-    var diaSem = a.getDay()
-
+    escreva.innerHTML = `Agora são exatamente ${hora} horas de uma ` 
     switch(diaSem) {
         case 0:
-         console.log('Domingo')
-        break    
+            escreva.innerHTML += 'Domingo-feira. '
+            break    
         case 1:
-            console.log('Segunda')
+            escreva.innerHTML +='Segunda-feira. '
             break
         case 2:
-            console.log('Terça')
+            escreva.innerHTML +='Terça-feira. '
             break
         case 3:
-            console.log('Quarta')
+            escreva.innerHTML +='Quarta-feira. '
             break
         case 4:
-            console.log('Quinta')
+            escreva.innerHTML +='Quinta-feira. '
             break
         case 5:
-            console.log('Sexta')
+            escreva.innerHTML +='Sexta-feira. '
             break
         case 7:
-            console.log('Sábado')
+            escreva.innerHTML +='Sábado-feira. '
             break
         default:
             break
     }
+
+    if (hora>=6 && hora<9) {
+        escreva.innerHTML += ' Bom dia!'
+        insira.src = 'imagens/a-manha1.png'
+        document.body.style.background = '#f7ae04b2'
+
+    } else if (hora>=9 && hora<12) {
+        escreva.innerHTML += ' Bom dia!'
+        insira.src = 'imagens/a-manha2.png'
+        document.body.style.background = '#f7ae04b2'
+
+    } else if (hora>=12 && hora<=15) {
+        escreva.innerHTML += ' Boa tarde!'
+        insira.src = 'imagens/a-tarde1.png'
+        document.body.style.background = '#1C3240'
+
+    } else if (hora>15 && hora<18) {
+        escreva.innerHTML += ' Boa tarde!'
+        insira.src = 'imagens/a-tarde2.png'
+        document.body.style.background = '#1C3240'
+
+    } else if (hora>=18 && hora<=23) {
+        escreva.innerHTML += ' Boa noite!'
+        insira.src = 'imagens/a-noite1.png'
+        document.body.style.background = '#0D212C'
+
+    } else {
+        escreva.innerHTML += ' Boa madrugada!'
+        insira.src = 'imagens/a-noite2.png'
+        document.body.style.background = '#0a1922'
+    }    
 }
